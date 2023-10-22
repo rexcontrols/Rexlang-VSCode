937afbbc11aa32c0b25a85fb311c0509bbb7e7cf
Jan Gebhart <gebhart@rexcontrols.cz>
2023-10-22 20:37:26 +0200

    release 0.0.11

--- rexlang.h
+++ rexlang.h
@@ -1,4 +1,5 @@
 #ifdef __DATE__ //rexlang nemá definici __DATE__ a díky tomu neinkluduje nic z tohoto soubnoru
+#ifdef REXLANG
 
 #pragma warning disable 4267
 #define DISABLE_INTELLIGENCE_ERRORS
@@ -13,4 +14,5 @@
 
 #undef DISABLE_INTELLIGENCE_ERRORS
 #pragma clang diagnostic pop
+#endif
 #endif
